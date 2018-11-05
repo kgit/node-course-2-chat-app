417a242f8e9817f1bb3ab2a29bbdcbfc329944a6
KJ <web@keakaj.com>
2018-11-04 17:49:32 -0700

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -17,13 +17,31 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: Date.now()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: Date.now()
+  });
+
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
+
     io.emit('newMessage', {
       from: message.from,
       text: message.text,
       createdAt: Date.now()
     });
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: Date.now()
+    // });
   });
 
   socket.on('disconnect', () => {
